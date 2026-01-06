b002a0629efb4afa083f5fc9eabfd72459e15a9e
Dominick Lebo <domlebo1@gmail.com>
2026-01-05 22:34:02 -0500

    jump to end or begining of input field.

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -1,47 +1,50 @@
-// Surfingkeys configuration - vulcan-mode edition
-// Last updated: January 2026
-// Controls: Ctrl+A = select all, Alt+0 = home, Alt+4 = end in insert mode
-
-// Disable default Ctrl+A (go to beginning of line)
 api.iunmap('<Ctrl-a>');
 
-// Ctrl+A → Select all text in input/textarea/contenteditable
-//api.imapkey('<Ctrl-a>', '#15Select all in current input', function() {
-//  var input = document.activeElement;
-//    if (input && (input.tagName === 'INPUT' || input.tagName === 'TEXTAREA' || input.isContentEditable)) {
-//        input.select();
-//    }
-//});
-
-// Alt+0 → Move cursor to beginning of line
-api.imapkey('<Alt-0>', '#15Move cursor to beginning of line', function() {
+// Alt-0 → Move cursor to very beginning of entire text input
+api.imapkey('<Alt-0>', '#15Move cursor to very beginning of text input', function() {
     var input = document.activeElement;
-    if (input && input.setSelectionRange) {
+    if (!input) return;
+
+    if (input.setSelectionRange) {
+        // Handles <input> and <textarea>
         input.setSelectionRange(0, 0);
         input.focus();
-    } else if (input && input.isContentEditable) {
-        var sel = window.getSelection();
-        sel.collapse(input.firstChild || input, 0);
-    }
-});
-
-// Alt+4 → Move cursor to end of line
-api.imapkey('<Alt-4>', '#15Move cursor to end of line', function() {
-    var input = document.activeElement;
-    if (input && input.setSelectionRange) {
-        input.setSelectionRange(input.value.length, input.value.length);
+    } else if (input.isContentEditable) {
+        // Handles contentEditable div/span etc.
         input.focus();
-    } else if (input && input.isContentEditable) {
         var sel = window.getSelection();
         var range = document.createRange();
-        range.selectNodeContents(input);
-        range.collapse(false);
+        range.setStart(input, 0);
+        range.setEnd(input, 0);
+        range.collapse(true);
         sel.removeAllRanges();
         sel.addRange(range);
     }
 });
 
-// ← Add more custom mappings, themes, search aliases, etc. here
-// Examples:
-// settings.theme = 'dark';  // or your preferred theme
-// api.mapkey('F', 'Hints in new tab', 'Hints("F")');
+// Alt-4 → Move cursor to very end of entire text input
+api.imapkey('<Alt-4>', '#15Move cursor to very end of text input', function() {
+    var input = document.activeElement;
+    if (!input) return;
+
+    if (input.setSelectionRange) {
+        // Handles <input> and <textarea>
+        var len = input.value.length;
+        input.setSelectionRange(len, len);
+        input.focus();
+    } else if (input.isContentEditable) {
+        // Handles contentEditable div/span etc.
+        input.focus();
+        var sel = window.getSelection();
+        var range = document.createRange();
+        var childCount = input.childNodes.length;
+        if (childCount > 0) {
+            range.setStartAfter(input.childNodes[childCount - 1]);
+        } else {
+            range.setStart(input, 0);
+        }
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+});
