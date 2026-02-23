069e97718f8ca22fe94b8f058d6637efd4ea94f4
Dominick Lebo <domlebo1@gmail.com>
2026-02-22 23:26:45 -0500

    add iunmap

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -1,4 +1,4 @@
-const { map, unmap, imap, imapkeyg, vunmap, mapkey, settings } = api;
+const { map, unmap, iunmap, imap, imapkeyg, vunmap, mapkey, settings } = api;
 imap('<Alt-a>', '<Ctrl-a>');
 iunmap('<Ctrl-a>');
 map('e', 'd');
