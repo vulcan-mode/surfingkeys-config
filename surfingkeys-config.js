07b57f38f9ac5f60f1f19430c8d01a61b674e928
Dominick Lebo <domlebo1@gmail.com>
2026-02-27 10:31:01 -0500

    fix

--- surfingkeys-config.js
+++ surfingkeys-config.js
@@ -3,6 +3,10 @@ api.iunmap('<Ctrl-a>');
 api.map('e', 'd');
 
 // Tab navigation
-api.map('<Ctrl-Alt-Left>', 'E');
-api.map('<Ctrl-Alt-Right>', 'R');
+api.mapkey('<Ctrl-Alt-ArrowLeft>', '#3Go to left tab', function() {
+    api.Normal.feedkeys('E');
+});
+api.mapkey('<Ctrl-Alt-ArrowRight>', '#3Go to right tab', function() {
+    api.Normal.feedkeys('R');
+});
 
